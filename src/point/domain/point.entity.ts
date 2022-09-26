d553fd73b9b75180c6c16133160653ab541c5a86
pino._.hoo <inhoo23@naver.com>
2022-09-26 18:46:40 +0900

    fix : point, quiz

--- src/point/domain/point.entity.ts
+++ src/point/domain/point.entity.ts
@@ -1,8 +1,16 @@
+import { User } from 'src/auth/domain/user.entity'
 import { BaseTimeEntity } from 'src/common/entity/BaseTime.Entity'
-import { Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Quiz } from 'src/quiz/domain/quiz.entity'
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity({ name: 'tbl_point' })
 export class Point extends BaseTimeEntity {
   @PrimaryGeneratedColumn()
   idx: number
+
+  @ManyToOne((type) => Quiz, (quiz) => quiz.point)
+  quiz: Quiz
+
+  @ManyToOne((type) => User, (user) => user.point)
+  user: User
 }
--- src/quiz/domain/quiz.entity.ts
+++ src/quiz/domain/quiz.entity.ts
@@ -1,8 +1,12 @@
 import { BaseTimeEntity } from 'src/common/entity/BaseTime.Entity'
-import { Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Point } from 'src/point/domain/point.entity'
+import { Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity({ name: 'tbl_quiz' })
 export class Quiz extends BaseTimeEntity {
   @PrimaryGeneratedColumn()
   idx: number
+
+  @OneToMany((type) => Point, (point) => point.quiz)
+  point: Point
 }
