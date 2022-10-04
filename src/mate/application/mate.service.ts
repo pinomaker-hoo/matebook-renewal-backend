ccf37af52e21e9befaf3a1f4b1fda852a8297c27
pino._.hoo <inhoo23@naver.com>
2022-10-04 11:04:37 +0900

    add : save Mate

--- src/mate/application/mate.service.ts
+++ src/mate/application/mate.service.ts
@@ -1,7 +1,19 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { User } from 'src/auth/domain/user.entity'
+import { Mate } from '../domain/mate.entity'
 import { MateRepository } from '../infrastructure/mate.repository'
 
 @Injectable()
 export class MateService {
   constructor(private readonly mateRepository: MateRepository) {}
+
+  async saveMate(user: User, name: string): Promise<Mate> {
+    try {
+      const mate: Mate = this.mateRepository.create({ user, name })
+      return await this.mateRepository.save(mate)
+    } catch (err) {
+      console.log(err)
+      throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST)
+    }
+  }
 }
--- src/mate/ui/mate.controller.ts
+++ src/mate/ui/mate.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common'
+import JwtGuard from 'src/auth/passport/auth.jwt.guard'
 import { MateService } from '../application/mate.service'
 
 @Controller('mate')
 export class MateController {
   constructor(private readonly mateService: MateService) {}
+
+  @Post()
+  @UseGuards(JwtGuard)
+  async saveMate(@Req() req, @Body() body) {
+    const { user } = req
+    const { name } = body
+    return await this.mateService.saveMate(user, name)
+  }
 }
