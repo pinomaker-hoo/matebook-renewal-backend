edf486e255844656d89e666a9cb5323e9efe7263
pino._.hoo <inhoo23@naver.com>
2022-10-04 11:32:11 +0900

    add : save Point service

--- src/point/application/point.service.ts
+++ src/point/application/point.service.ts
@@ -1,7 +1,18 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
+import { User } from 'src/auth/domain/user.entity'
 import { PointRepository } from '../infrastructure/point.repository'
+import { Point } from '../domain/point.entity'
 
 @Injectable()
 export class PointService {
   constructor(private readonly pointRepository: PointRepository) {}
+  async savePoint(user: User, point: number): Promise<Point> {
+    try {
+      const savePoint: Point = this.pointRepository.create({ user, point })
+      return await this.pointRepository.save(savePoint)
+    } catch (err) {
+      console.log(err)
+      throw new HttpException('ERROR', HttpStatus.BAD_REQUEST)
+    }
+  }
 }
