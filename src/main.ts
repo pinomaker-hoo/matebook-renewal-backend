0c21a558750fe643709db9ffeb67f08223b87285
pino._.hoo <inhoo23@naver.com>
2022-10-30 04:02:59 +0900

    fix : 요청 크기 수정

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as cookieParser from 'cookie-parser'
+import * as express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -12,6 +13,8 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
     credentials: true,
   })
+  app.use(express.json({ limit: '50mb' }))
+  app.use(express.urlencoded({ limit: '50mb', extended: true }))
   app.useGlobalPipes(new ValidationPipe())
   app.use(cookieParser())
   await app.listen(process.env.NODE_SERVER_PORT || 8003)
