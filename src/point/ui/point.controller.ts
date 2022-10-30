18486d4dc62533218ab8aef7274377dd24dd5dc7
pino._.hoo <inhoo23@naver.com>
2022-10-31 04:27:20 +0900

    add : add point api

--- src/point/ui/point.controller.ts
+++ src/point/ui/point.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common'
+import JwtGuard from 'src/auth/passport/auth.jwt.guard'
 import { PointService } from '../application/point.service'
 
 @Controller('point')
 export class PointController {
   constructor(private readonly pointService: PointService) {}
+
+  @Post()
+  @UseGuards(JwtGuard)
+  async savePoint(@Req() req, @Body() body) {
+    return await this.pointService.savePoint(req.user, body.point)
+  }
 }
