803b43c280adb78690973831c604ab0ec81577e3
pino._.hoo <inhoo23@naver.com>
2022-10-31 05:51:59 +0900

    add : userBook api controller

--- src/userBook/ui/userBook.controller.ts
+++ src/userBook/ui/userBook.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common'
+import JwtGuard from 'src/auth/passport/auth.jwt.guard'
 import { UserBookService } from '../application/userBook.service'
 
 @Controller('userBook')
 export class UserBookController {
   constructor(private readonly userBookService: UserBookService) {}
+
+  @Post('/:id')
+  @UseGuards(JwtGuard)
+  async readBook(@Req() req: any, @Param('id') id: string) {
+    return await this.userBookService.saveUserBook(req.user, Number(id))
+  }
+
+  @Get()
+  @UseGuards(JwtGuard)
+  async getReadBookList(@Req() req) {
+    return await this.userBookService.findUserBookListByUserIdx(req.user)
+  }
 }
