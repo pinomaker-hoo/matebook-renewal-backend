dd103076d71b48e5d0d5a0fa897e700d82b3f47e
pino._.hoo <inhoo23@naver.com>
2022-10-31 10:59:25 +0900

    add : comment api에 response 객체 추가

--- src/comment/ui/comment.controller.ts
+++ src/comment/ui/comment.controller.ts
@@ -8,6 +8,7 @@ import {
   UseGuards,
 } from '@nestjs/common'
 import JwtGuard from 'src/auth/passport/auth.jwt.guard'
+import { ApiResponse } from 'src/common/dto/api.response'
 import { CommentService } from '../application/comment.service'
 
 @Controller('comment')
@@ -19,11 +20,25 @@ export class CommentController {
   async saveComment(@Body() body, @Req() req, @Param('id') reviewIdx: string) {
     const { user } = req
     const { text } = body
-    return await this.commentService.saveComment(user, text, Number(reviewIdx))
+    const response = await this.commentService.saveComment(
+      user,
+      text,
+      Number(reviewIdx),
+    )
+    return ApiResponse.of({
+      data: response,
+      message: 'Success Save Comment',
+      statusCode: 200,
+    })
   }
 
   @Get('/:id')
   async getComment(@Param('id') reviewIdx: string) {
-    return await this.commentService.getCommentList(Number(reviewIdx))
+    const response = await this.commentService.getCommentList(Number(reviewIdx))
+    return ApiResponse.of({
+      data: response,
+      message: 'Success Find Comment',
+      statusCode: 200,
+    })
   }
 }
