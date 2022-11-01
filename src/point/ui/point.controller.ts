d63a78209df0f6faad5a11c0dcba48f4d78126f9
pino._.hoo <inhoo23@naver.com>
2022-11-01 11:44:27 +0900

    add : get Point api

--- src/point/ui/point.controller.ts
+++ src/point/ui/point.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common'
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common'
 import JwtGuard from 'src/auth/passport/auth.jwt.guard'
 import { ApiResponse } from 'src/common/dto/api.response'
 import { PointService } from '../application/point.service'
@@ -14,6 +14,17 @@ export class PointController {
     return ApiResponse.of({
       data: response,
       message: 'Success Find Mate and Point',
+      statusCode: 200,
+    })
+  }
+
+  @Get()
+  @UseGuards(JwtGuard)
+  async getPoint(@Req() req) {
+    const response = await this.pointService.getSumPoint(req.user)
+    return ApiResponse.of({
+      data: response,
+      message: 'Success Find Point',
       statusCode: 200,
     })
   }
